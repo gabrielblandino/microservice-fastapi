f103ae25803868e98d4605980c18b83cb499a178
Gabriel Blandino <gabrielblandinocs@gmail.com>
2025-12-25 14:55:15 -0300

    fix: create files and redis

--- inventory/main.py
+++ inventory/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connections, HashModel
 import os
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['https://localhost:3000'],
+    allow_methods=['*'],
+    allow_hearders=['*']
+)
 
 redis = get_redis_connections(
     host=os.getenv('host'),
@@ -21,4 +29,4 @@ class Product(HashModel):
 
 @app.get('/products')
 def all():
-    return[]
+    return Product.all_pks()
